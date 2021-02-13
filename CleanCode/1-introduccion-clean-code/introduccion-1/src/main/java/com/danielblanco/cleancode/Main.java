ee3399105d21c1415911347c8155d89061e5f88f
Daniel Blanco <dablancal96@gmail.com>
2021-02-13 12:59:34 +0100

    Refactor first example

--- CleanCode/1-introduccion-clean-code/introduccion-1/src/main/java/com/danielblanco/cleancode/Main.java
+++ CleanCode/1-introduccion-clean-code/introduccion-1/src/main/java/com/danielblanco/cleancode/Main.java
@@ -2,9 +2,9 @@ package com.danielblanco.cleancode;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Calculadora o = new Calculadora();
-		System.out.println("Resultado: " + o.calcular(5.0, 4.0));
-	}
+    public static void main(String[] args) {
+        Calculadora c = new Calculadora();
+        System.out.println("Resultado: " + c.calcular(5.0, 4.0));
+    }
 
 }
