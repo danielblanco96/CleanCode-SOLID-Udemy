e45bbd5925a6afc1e02dc0fbc06cde0b46c1e37e
Daniel Blanco <dablancal96@gmail.com>
2021-02-13 20:22:28 +0100

    Fix error on DIP example

--- SOLID/5-DIP/src/main/java/dip/refactored/Company.java
+++ SOLID/5-DIP/src/main/java/dip/refactored/Company.java
@@ -1,18 +1,14 @@
 package dip.refactored;
 
-import java.util.List;
-
 import dip.refactored.factory.EmployeeePersistenceFactory;
 import dip.refactored.persistence.EmployeePersistence;
 
 public class Company {
 
-    private List<Employee> employees;
     private EmployeePersistence persistence;
 
     public Company(EmployeeePersistenceFactory persistenceFactory) {
         persistence = persistenceFactory.getEmployeePersistance();
-        employees = persistence.findAll();
     }
 
     public void addEmployee(Employee e) {
